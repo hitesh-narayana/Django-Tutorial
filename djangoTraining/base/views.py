796dce191568023f9a61bc15b0fb5ef6e4d35164
Hitesh <hiteshna@atrihub.io>
2025-02-12 08:46:07 -0800

    HN_ICTRAINING-1807:  Models and Admin  [#ICTRAINING-1807(state):done]

--- djangoTraining/base/views.py
+++ djangoTraining/base/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Room
+
 # Create your views here.
 
-rooms=[
-    {'id':1, 'name':'Room 1', 'status':'Open'},
-    {'id':2, 'name':'Room 2', 'status':'Open'},
-    {'id':3, 'name':'Room 3', 'status':'Closed'},
-    {'id':4, 'name':'Room 4', 'status':'Open'},
-    {'id':5, 'name':'Room 5', 'status':'Closed'},
-]
+# rooms=[
+#     {'id':1, 'name':'Room 1', 'status':'Open'},
+#     {'id':2, 'name':'Room 2', 'status':'Open'},
+#     {'id':3, 'name':'Room 3', 'status':'Closed'},
+#     {'id':4, 'name':'Room 4', 'status':'Open'},
+#     {'id':5, 'name':'Room 5', 'status':'Closed'},
+# ]
 
 def home(request):
+   rooms = Room.objects.all()
    context = {'rooms':rooms}
    return render(request, 'home.html', context)
 
